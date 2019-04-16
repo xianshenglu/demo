c4ad969c8f03dd17faa17347c5f9a8679f8f1610
xianshenglu <xianshenglu@foxmail.com>
2019-04-16 22:42:46 +0800

    add ts-vue-components-demo

---
+++ demo/vue/ts-vue-components-demo/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <SelectSupportAll/>
+  </div>
+</template>
+
+<script>
+import TsVueComponets from 'ts-vue-components/src/components/index'
+const { SelectSupportAll } = TsVueComponets
+console.log(SelectSupportAll)
+
+export default {
+  name: 'app',
+  components: {
+    SelectSupportAll
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
