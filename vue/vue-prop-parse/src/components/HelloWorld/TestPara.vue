74c1c244736b470b1282e407dd43e26dcf7d70cf
luxiansheng <luxiansheng@zto.cn>
2019-07-06 11:20:18 +0800

    add demo

---
+++ vue/vue-prop-parse/src/components/HelloWorld/TestPara.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="TestPara"></div>
+</template>
+
+<script>
+export default {
+  name: 'TestPara',
+  props: [
+    'attr-aa',
+    'attrBb',
+    'attr-Cc', // undefined
+    // 'attrCc', // undefined
+    // 'attr-cc', // undefined
+    // 'attrcc', // undefined
+    'Attr-Dd', // AttrDd undefined,
+    // 'AttrDd', // AttrDd undefined
+    // 'Attr-dd', // AttrDd undefined
+    // 'Attrdd', // Attrdd undefined
+    // 'attrDd', // attrDd undefined
+    'AttrEe',
+    'attr_ff',
+    'attr_Gg',
+    'Attr_Hh',
+    '_attr_ii',
+    '_attr_Jj',
+    '_attrKk',
+    '_AttrLl'
+  ],
+  created () {
+    window.vm = this
+    console.log(vm.$props)
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
---
+++ vue/vue-prop-parse/src/components/HelloWorld/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="HelloWorld">
+    <TestPara
+      :attr-aa="0"
+      :attr-bb="1"
+      :attr-Cc="2"
+      :Attr-Dd="3"
+      :AttrEe="4"
+      :attr_ff="5"
+      :attr_Gg="6"
+      :Attr_Hh="7"
+      :_attr_ii="8"
+      :_attr_Jj="9"
+      :_attrKk="10"
+      :_AttrLl="11"
+    />
+  </div>
+</template>
+
+<script>
+import TestPara from './TestPara'
+export default {
+  name: 'HelloWorld',
+  components: {
+    TestPara
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
