855d5ae616812142e649c9ab3f8de1ec865e3b47
xianshenglu <xianshenglu@qq.com>
2022-05-13 21:19:51 +0800

    feat: add spring-web demo

---
+++ java/spring-web/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		try {
+			SpringApplication.run(DemoApplication.class, args);
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
